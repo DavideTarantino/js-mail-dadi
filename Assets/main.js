2d376d4cdf562e14ee16fd76c2d3e5d75267101b
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-10 15:49:25 +0100

    inserimento mingioco 2

--- Assets/main.js
+++ Assets/main.js
@@ -1,3 +1,5 @@
+//Minigioco 1
+
 let x = Math.floor((Math.random() * 100) + 1);
 document.getElementById("primo").innerHTML = x;
 
@@ -9,3 +11,17 @@ if(x > y){
 }else{
     document.getElementById("risultato").innerHTML = "HAI PERSO :("
 }
+
+let email = ["mariorossi@gmail.com","alfredomariano@gmail.com","davidetarantino@gmail.com","luigipucci@gmail.com"]
+let inputmail = document.getElementById('inputmail').value;
+
+function accedi(){
+    if(inputmail == email){
+        document.getElementById("responso").innerHTML = "ACCESSO CONSENTITO :)";
+    }else{
+        document.getElementById("responso").innerHTML = "ACCESSO NEGATO !!";
+    }    
+}
+
+  
+    
